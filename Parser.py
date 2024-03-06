e9110d40eab0a494fc23a18199e8fe4396f00c67
Alex <leshakochetcov@gmail.com>
2024-03-06 16:19:53 +0300

    Сделал асинхронное всё, что мог

--- Parser.py
+++ Parser.py
@@ -1,9 +1,9 @@
-import aiohttp, json, os
+import aiohttp, json, os, typing
 import asyncio, requests, aiofiles
 from bs4 import BeautifulSoup
 
 
-async def main(path):
+async def main(path: str):
     async with aiohttp.ClientSession() as session:
         async with session.get(path) as response:
             print("Status:", response.status)
@@ -12,29 +12,23 @@ async def main(path):
             return html
 
 
-"""
-https://api.thecatapi.com/v1/images/search - cat
-https://random.dog/woof.json - dog
-"""
-
-
 async def save_dog_pic(page: json, count: int, path: str):
+    if not os.path.exists(path):
+        os.mkdir(path)
     file = json.loads(page)
     print(file['url'])
     link = file['url'].rfind('.')
     # response = requests.get(file['url'])
-    if not os.path.exists(path):
-        os.mkdir(path)
     async with aiohttp.ClientSession() as session:
         async with session.get(file['url']) as response:
             async with aiofiles.open(f'{path}\\dog{count}{file["""url"""][link:]}', 'wb') as file:
                 async for data, _ in response.content.iter_chunks():
                     await file.write(data)
-    print("Done")
 
 
 async def save_cat_pic(page: str, count: int, path: str):
     file = json.loads(page)
+    # print(page)
     print(file[0]['url'])
     link = file[0]['url'].rfind('.')
     # response = requests.get(file['url'])
@@ -46,34 +40,55 @@ async def save_cat_pic(page: str, count: int, path: str):
                 async for data, _ in response.content.iter_chunks():
                     await file.write(data)
 
-    print("Done")
+
+async def do_task_main(path: str, quantity: int):
+    tasks = []
+    for i in range(quantity):
+        task = asyncio.create_task(main(path))
+        tasks.append(task)
+    res = await asyncio.gather(*tasks)
+    print(res, type(res))
+    return res
+
+
+async def do_tasks_save_dog(res: list):
+    tasks = []
+    for i in range(len(res)):
+        task = asyncio.create_task(save_dog_pic(page=res[i], count=i + 1, path=path))
+        tasks.append(task)
+        # print(i)
+    await asyncio.gather(*tasks)
+
+
+async def do_tasks_save_cat(res: list):
+    tasks = []
+    for i in range(len(res)):
+        task = asyncio.create_task(save_cat_pic(page=res[i], count=i + 1, path=path))
+        tasks.append(task)
+    # print(i)
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
+    cat = "https://api.thecatapi.com/v1/images/search"
+    dog = "https://random.dog/woof.json"
     user = input('cat / dog / all? \n')
     quantity = int(input("quantity: "))
     path = input("path: ")
     if user.lower() == 'dog':
-        count = 1
-        for i in range(quantity):
-            page = asyncio.run(main("https://random.dog/woof.json"))
-            asyncio.run(save_dog_pic(page=page, count=count, path=path))
-            count += 1
+        res = asyncio.run(do_task_main(dog, quantity))
+        # print(res)
+        asyncio.run(do_tasks_save_dog(res))
     elif user.lower() == 'cat':
-        count = 1
-        for i in range(quantity):
-            page = asyncio.run(main("https://api.thecatapi.com/v1/images/search"))
-            asyncio.run(save_cat_pic(page=page, count=count, path=path))
-            count += 1
+        res = asyncio.run(do_task_main(cat, quantity))
+        # print(res)
+        asyncio.run(do_tasks_save_cat(res))
     elif user.lower() == 'all':
-        count = 1
-        for i in range(quantity // 2):
-            cats = asyncio.run(main("https://api.thecatapi.com/v1/images/search"))
-            dogs = asyncio.run(main("https://random.dog/woof.json"))
-            asyncio.run(save_cat_pic(page=cats, count=count, path=path))
-            asyncio.run(save_dog_pic(page=dogs, count=count, path=path))
-            count += 1
-
+        res_dog = asyncio.run(do_task_main(path=dog, quantity=quantity // 2))
+        res_cat = asyncio.run(do_task_main(path=cat, quantity=quantity // 2))
+        asyncio.run(do_tasks_save_dog(res_dog))
+        asyncio.run(do_tasks_save_cat(res_cat))
+    print("Done")
 """ 
 print(page)
 soup = BeautifulSoup(page, 'html.parser')
